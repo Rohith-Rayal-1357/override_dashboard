d284eae6deb7392535c4a4773270c7d8793c0a8c
Rohith-Rayal-1357 <136075177+Rohith-Rayal-1357@users.noreply.github.com>
2025-03-27 15:45:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -237,8 +237,8 @@ if not module_ref_df.empty:
                 # Use Streamlit's data editor with the editable column
                 edited_df = st.data_editor(
                     source_df,
-                    key=f"data_editor_{selected_table}_{editable_column}",
-                    #num_rows="dynamic",
+                    #key=f"data_editor_{selected_table}_{editable_column}",
+                    num_rows="dynamic",
                     use_container_width=True,
                     disabled=[col for col in source_df.columns if col != editable_column]
                 )
